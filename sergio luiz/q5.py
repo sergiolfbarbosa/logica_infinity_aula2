5438ba8995833c02e191490263a34e4b143e1c77
sergiolfbarbosa <146891722+sergiolfbarbosa@users.noreply.github.com>
2023-10-10 19:00:49 -0300

    Add files via upload

---
+++ sergio luiz/q5.py
@@ -0,0 +1,9 @@
+var1 = float(input("Digite a nota: "))
+var2 = float(input("Digite a segunda nota: "))
+
+if var1 > var:
+    print(f"O {numero1} é maior {numero2}")
+elif numero1 < numero2:
+    print(f"O {numero2} é maior {numero1}")
+else:
+    print(f"Os valores digitados são iguas.")
